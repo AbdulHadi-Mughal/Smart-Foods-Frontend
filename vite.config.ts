7b54e6e94dc71cee101eb06056e5cfb36c22aff1
Abdul Hadi Mughal <abdulhadimughalmunch@gmail.com>
2025-08-09 08:37:59 +0500

    Removed visualizer

--- vite.config.ts
+++ vite.config.ts
@@ -2,19 +2,19 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
 import tailwindcss from "@tailwindcss/vite";
-import { visualizer } from "rollup-plugin-visualizer";
+//import { visualizer } from "rollup-plugin-visualizer";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
-    visualizer({
-      open: true, // Automatically opens report in browser
-      filename: "stats.html", // Output file
-      gzipSize: true,
-      brotliSize: true,
-    }),
+    // visualizer({
+    //   open: true, // Automatically opens report in browser
+    //   filename: "stats.html", // Output file
+    //   gzipSize: true,
+    //   brotliSize: true,
+    // }),
   ],
   resolve: {
     alias: {
